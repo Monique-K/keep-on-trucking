70ec7e5237c61cff76b5afbbe99fa5819df7099f
Monique-K <mkhoury87@hotmail.com>
2019-01-12 11:58:56 -0500

    connected to api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allStops: []
+    }
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:8080/stops', {
+      method: 'GET', 
+    })
+    .then(function(response) {
+      console.log("received")
+      response.text()
+      .then(function(data) {
+        console.log("DATA:", data);
+      });
+    })
+  }
+    // .then(function(myJson) {
+    //   console.log("myjson", myJson)
+    //   console.log(JSON.stringify(myJson));
+    // })
+    // .then(results => {
+    //   console.log("results", results)
+    //   const allStops = results;
+    //   this.setState({allStops: [...this.state.allStops, allStops]})
+    //   return allStops;
+    // }).then(data => {
+    //   console.log("dataaaaa", data)
+      // data.forEach(person => {
+      //   this.setState({ 
+      //     isLoaded: true,
+      //     candidates: [...this.state.candidates, person] })
+      // })
+    // })
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
