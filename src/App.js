736234b7aefe5a578e7294c90b2cae24f9059245
Monique-K <mkhoury87@hotmail.com>
2019-01-13 20:15:14 -0500

    added api to file structure

--- src/App.js
+++ src/App.js
@@ -1,48 +1,104 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+const stopsJson = require('./json-api/stops');
+const legsJson = require('./json-api/legs');
+const driverJson = require('./json-api/driver');
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      allStops: []
+      allStops: stopsJson,
+      legs: legsJson,
+      driver: driverJson
     }
   }
 
-  componentWillMount() {
-    fetch('http://localhost:8080/stops', {
-      method: 'GET', 
-    })
-    .then(function(response) {
-      console.log("received")
-      response.text()
-      .then(function(data) {
-        console.log("DATA:", data);
-      });
+  // getStops() {
+  //   fetch('http://localhost:8080/stops', {
+  //     method: 'GET', 
+  //   })
+  //   .then(function(response) {
+  //     response.text()
+  //     .then(function(data) {
+  //       console.log("stops:", JSON.parse(data))
+  //       return JSON.parse(data)
+  //     });
+  //   })
+  // }
+
+  // getLegs() {
+  //   fetch('http://localhost:8080/legs', {
+  //     method: 'GET', 
+  //   })
+  //   .then(function(response) {
+  //     response.text()
+  //     .then(function(data) {
+  //       console.log("legs:", JSON.parse(data))
+  //       return JSON.parse(data)
+  //     });
+  //   })
+  // }
+
+  // getDriver() {
+  //   fetch('http://localhost:8080/driver', {
+  //     method: 'GET', 
+  //   })
+  //   .then(function(response) {
+  //     response.text()
+  //     .then(function(data) {
+  //       console.log("driver:", JSON.parse(data))
+  //       return JSON.parse(data)
+  //     });
+  //   })
+  // }
+
+  // componentWillMount() {
+  //   const stops = this.getStops();
+  //   const legs = this.getLegs();
+  //   const driver = this.getDriver();
+  //   this.setState({
+  //     allStops: [...this.state.allStops, stops],
+  //     legs: [...this.state.legs, legs],
+  //     driver: {0: driver}
+  //   });
+  // }
+  
+  makeGrid() {
+    const grid = []
+    for (let i = 0; i < 10; i++) {
+      let id = "box" + i 
+      grid.push(<div key={id} className={`box ${id}`}></div>)
+      }
+      console.log(grid)
+      grid.forEach(div => {
+        return div
+      })
+      // for (let i = 0; i < grid.length; i++) {
+      //   if (i) {
+      //     return grid[i]
+      //   }
+      // }
+  }
+
+  dropDown() {
+    const items = this.state.allStops
+    items.forEach(item => {
+      return (<option value={item.name} x={item.x} y={item.y}>{item.name}</option>)
     })
   }
-    // .then(function(myJson) {
-    //   console.log("myjson", myJson)
-    //   console.log(JSON.stringify(myJson));
-    // })
-    // .then(results => {
-    //   console.log("results", results)
-    //   const allStops = results;
-    //   this.setState({allStops: [...this.state.allStops, allStops]})
-    //   return allStops;
-    // }).then(data => {
-    //   console.log("dataaaaa", data)
-      // data.forEach(person => {
-      //   this.setState({ 
-      //     isLoaded: true,
-      //     candidates: [...this.state.candidates, person] })
-      // })
-    // })
-  
 
   render() {
     return (
       <div className="App">
+        <div className="map">
+          {this.makeGrid()}
+        </div>
+        <div className="drop-down">
+        <select>
+          {this.dropDown()}
+        </select>
+        </div>
       </div>
     );
   }
